fc284a06ef56e8bc3e93f997fea90a76306ad65e
changwangss <chang1.wang@intel.com>
2024-06-25 02:31:27 -0700

    fix install
    Signed-off-by: changwangss <chang1.wang@intel.com>

--- tests/test_ragas.py
+++ tests/test_ragas.py
@@ -19,8 +19,9 @@ class TestRagasMetric(unittest.TestCase):
 
     # Replace this with the actual retrieved context from your RAG pipeline
     retrieval_context = ["All customers are eligible for a 30 day full refund at no extra cost."]
-
-    metric = RagasMetric(threshold=0.5, model="http://localhost:8008")
+    from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+    embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+    metric = RagasMetric(threshold=0.5, model="http://localhost:8008", embeddings=embeddings)
     test_case = {
         "input": "What if these shoes don't fit?",
         "actual_output": actual_output,
