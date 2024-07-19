38dbb67aa917bcf78ebab4bd29befd8f0762ff1c
Xinyu Ye <xinyu.ye@intel.com>
2024-07-19 09:55:50 +0800

    added support for CRUD evaluation.
    Signed-off-by: Xinyu Ye <xinyu.ye@intel.com>

---
+++ evals/evaluation/crud_rag/evaluate.py
@@ -0,0 +1,156 @@
+import datetime
+import json
+import os
+import requests
+from .metrics import bleu_score, rougeL_score, LLM_score
+from tqdm import tqdm
+
+class CRUD_Evaluator:
+    def __init__(self, dataset: list[dict], output_path: str, task: str) -> None:
+        """Args:
+            dataset (list[dict]): The dataset for evaluation.
+            output_path (str): The path to save results.
+            task (str): Task to evaluate.
+        """
+        self.task = task
+        self.output_path = output_path
+        self.dataset = dataset
+
+    def scoring(self, data: dict) -> dict:
+        generated_text = data["generated_text"]
+        if self.task == "summarization":
+            ground_truth_text = data["summary"]
+        elif self.task == "question_answering":
+            ground_truth_text = data["answers"]
+        elif self.task == "continuation":
+            ground_truth_text = data["continuing"]
+        elif self.task == "hallucinated_modified":
+            ground_truth_text = data["hallucinatedMod"]
+        else:
+            raise NotImplementedError(
+                f"Unknown task {self.task}, only support "
+                "summarization, question_answering, continuation and hallucinated_modified."
+            )
+        data["ground_truth_text"] = ground_truth_text
+
+        bleu_avg, bleu1, bleu2, bleu3, bleu4 = bleu_score(generated_text, ground_truth_text)
+
+        return {
+            'metrics': {
+                'bleu-avg': bleu_avg or 0.0,
+                'bleu-1': bleu1 or 0.0,
+                'bleu-2': bleu2 or 0.0,
+                'bleu-3': bleu3 or 0.0,
+                'bleu-4': bleu4 or 0.0,
+                'rouge-L': rougeL_score(generated_text, ground_truth_text) or 0.0,
+                'LLM-score': LLM_score(generated_text, ground_truth_text) or 0.0,
+                'length': len(generated_text)
+            },
+            'log': {
+                'generated_text': generated_text,
+                'ground_truth_text': ground_truth_text,
+                'evaluateDatetime': str(datetime.datetime.now()),
+            },
+            'valid': len(generated_text.strip()) != 0
+        }
+
+    def compute_overall(self, results: list[dict]) -> dict:
+        overall = {'bleu-avg': 0, 'bleu-1': 0, 'bleu-2': 0, 'bleu-3': 0, 
+                    'bleu-4': 0, 'rouge-L': 0, 'LLM-score': 0.0, 'length': 0}
+
+        for result in results:
+            overall = {key: overall[key] + result['metrics'][key] for key in overall.keys()}
+
+        overall_save = {f"avg. {key}": value / len(results) for key, value in overall.items()}
+
+        overall_save['num'] = len(results)
+        
+        return overall_save
+
+
+    def save_output(self, output: dict) -> None:
+        """Save evaluation results."""
+        with open(self.output_path, 'w', encoding='utf-8') as f:
+            json.dump(output, f, ensure_ascii=False, indent=4)
+    
+    def read_output(self) -> dict:
+        with open(self.output_path) as f:
+            return json.load(f)
+
+    def remove_invalid(self, results: list[dict]) -> list[dict]:
+        """Remove invalid results from the list and return the cleaned results."""
+        return [result for result in results if result['valid']]
+
+    def send_request(self, data, arguments):
+        service_url = arguments.service_url
+        headers = {"Content-Type": "application/json"}
+        if self.task == "summarization":
+            query = data["text"]
+        elif self.task == "question_answering":
+            query = data["questions"]
+        elif self.task == "continuation":
+            query = data["beginning"]
+        elif self.task == "hallucinated_modified":
+            query = data["newsBeginning"]
+        else:
+            raise NotImplementedError(
+                f"Unknown task {self.task}, only support "
+                "summarization, question_answering, continuation and hallucinated_modified."
+            )
+        json_data = {}
+        json_data["messages"] = query
+        json_data["temperature"] = arguments.temperature
+        json_data["max_new_tokens"] = arguments.max_new_tokens
+        json_data = json.dumps(json_data)
+        response = requests.post(service_url, data=json_data, headers=headers)
+        return response
+
+    def evaluate(self, arguments, sort=True, show_progress_bar=False, contain_original_data=False):
+        """Run a complete evaluation.
+        
+        Args:
+            dataset (list[dict]): The dataset for evaluation.
+            output_path (str): The path to save results.
+            task (str): Task to evaluate.
+            arguments: Arguments.
+            sort (bool): Whether to sort the results by id.
+            show_progress_bar (bool): Whether to display a progress bar.
+            contain_original_data (bool): Whether to include original data in the results for debugging.
+        
+        Returns:
+            dict: Output dictionary contains fields such as: overall, results, etc.
+        """
+        if os.path.exists(self.output_path):  # Resume evaluation
+            results = self.read_output(self.output_path).get('results', [])
+            results = self.remove_invalid(results)
+            saved_ids = [result['id'] for result in results]
+        else:
+            results = []
+            saved_ids = []
+
+        for data in (tqdm(self.dataset) if show_progress_bar else self.dataset):
+            if data['ID'] in saved_ids:
+                continue  # Skip results that have already been evaluated and are valid
+            try:
+                generated_text = self.send_request(data, self.task, arguments)
+                data["generated_text"] = generated_text
+                result = {'id': data['ID'], **self.scoring(data, self.task)}
+                if contain_original_data:
+                    result['original_data'] = data
+                results.append(result)
+            except Exception as e:
+                print(repr(e))
+
+        results = sorted(results, key=lambda x: x['id']) if sort else results
+        valid_results = self.remove_invalid(results)
+
+        try:
+            overall = self.compute_overall(valid_results) if len(valid_results) > 0 else {}
+        except Exception as e:
+            print(repr(e))
+            overall = dict()
+
+        output = {'overall': overall, 'results': results}
+        self.save_output(self.output_path, output)
+        print(f"Output saved to {self.output_path}!")
+        return output
---
+++ evals/evaluation/crud_rag/metrics/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from .metrics import bleu_score, rougeL_score, LLM_score
