a61efa8f127297e6871d9cfe3a18ecd0e1548530
minmin-intel <minmin.hou@intel.com>
2024-08-26 09:59:45 -0700

    docker container for crag eval

---
+++ evals/evaluation/crag_eval/docker/build_image.sh
@@ -0,0 +1,9 @@
+dockerfile=Dockerfile
+
+docker build \
+    -f ${dockerfile} . \
+    -t crag-eval:latest \
+    --network=host \
+    --build-arg http_proxy=${http_proxy} \
+    --build-arg https_proxy=${https_proxy} \
+    --build-arg no_proxy=${no_proxy} \
