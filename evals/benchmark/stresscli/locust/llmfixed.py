c62f52f095a73d21f41b304904a3a4bf64f0e4b3
lvliang-intel <liang1.lv@intel.com>
2024-08-28 16:05:36 +0800

    Align llm microservice parameters with end to end test
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- evals/benchmark/stresscli/locust/llmfixed.py
+++ evals/benchmark/stresscli/locust/llmfixed.py
@@ -12,6 +12,12 @@ def getReqData():
     return {
         "query": "What is the revenue of Nike in last 10 years before 2023? Give me detail",
         "max_new_tokens": 128,
+        "top_k": 10,
+        "top_p":0.95,
+        "typical_p": 0.95,
+        "temperature":0.01,
+        "repetition_penalty":1.03,
+        "streaming": True
     }
 
 
