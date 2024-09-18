43cab8bca33528230ff8aa74043a5d90abe2eab5
CharleneHu-42 <yabai.hu@intel.com>
2024-09-18 16:01:44 +0800

    support run-time limit in poisson load shape

--- evals/benchmark/stresscli/locust/poisson_load_shape.py
+++ evals/benchmark/stresscli/locust/poisson_load_shape.py
@@ -25,18 +25,22 @@ class PoissonLoadShape(LoadTestShape):
         if self.last_tick == 0:
             logger.info("Poisson load shape arrival rate: {arrival_rate}".format(arrival_rate=self.arrival_rate))
 
-        run_time = self.get_run_time()        
-        time_diff = run_time - self.last_tick
-        self.last_tick = run_time
+        run_time = self.get_run_time()
+        if run_time < self.runner.environment.parsed_options.run_time:
+            time_diff = run_time - self.last_tick
+            self.last_tick = run_time
 
-        new_users = np.random.poisson(lam=self.arrival_rate * time_diff)
-        current_users=self.get_current_user_count()
-        user_count = current_users + new_users
-        logger.debug(
-            "Current users: {current_users}, New users: {new_users}, Target users: {target_users}".format(
-                current_users=current_users, new_users=new_users, target_users=user_count
+            new_users = np.random.poisson(lam=self.arrival_rate * time_diff)
+            current_users=self.get_current_user_count()
+            user_count = current_users + new_users
+            logger.debug(
+                "Current users: {current_users}, New users: {new_users}, Target users: {target_users}".format(
+                    current_users=current_users, new_users=new_users, target_users=user_count
+                )
             )
-        )
-        # Avoid illegal spawn_rate value of 0
-        spawn_rate = max(0.01, new_users)
-        return (user_count, spawn_rate)
+            # Avoid illegal spawn_rate value of 0
+            spawn_rate = max(0.01, new_users)
+            return (user_count, spawn_rate)
+
+        self.runner.environment.stop_timeout = 0
+        return None
