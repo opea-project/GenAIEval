1047133da8a6024fb9e8b4578a30a7a8ac470595
aasavari <aasavari.dhananjay.kakne@intel.com>
2024-09-25 22:55:59 +0000

    testing automatic validation of ragas metrics
    Signed-off-by: aasavari <aasavari.dhananjay.kakne@intel.com>

--- tests/test_ragas.py
+++ tests/test_ragas.py
@@ -25,15 +25,20 @@ class TestRagasMetric(unittest.TestCase):
 
         # Replace this with the actual retrieved context from your RAG pipeline
         retrieval_context = ["All customers are eligible for a 30 day full refund at no extra cost."]
+        reference_context = ["We can only process full refund upto 30 day after the purchase."]
         from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
         embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-base-en-v1.5")
-        metric = RagasMetric(threshold=0.5, model=f"http://{host_ip}:{port}", embeddings=embeddings)
+
+        metric = RagasMetric(threshold=0.5, 
+                                model=f"http://{host_ip}:{port}", 
+                                embeddings=embeddings)
         test_case = {
             "question": ["What if these shoes don't fit?"],
             "answer": [actual_output],
             "ground_truth": [expected_output],
             "contexts": [retrieval_context],
+            "reference_contexts": [reference_context]
         }
 
         metric.measure(test_case)
