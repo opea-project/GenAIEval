7695ad31951ac74a07d94eb7f2f99b41d06d18c6
minmin-intel <minmin.hou@intel.com>
2024-11-14 16:05:34 -0800

    update code for conv rag

--- evals/evaluation/agent_eval/crag_eval/run_benchmark/run_conv_rag.sh
+++ evals/evaluation/agent_eval/crag_eval/run_benchmark/run_conv_rag.sh
@@ -5,7 +5,9 @@ FILEDIR=$WORKDIR/datasets/ragagent_eval/
 FILENAME=crag_qa_music.jsonl
 OUTPUT=$WORKDIR/datasets/ragagent_eval/val_conv_rag_music_full.jsonl
 
-python3 benchmark.py \
+export RETRIEVAL_TOOL_URL="http://${host_ip}:8889/v1/retrievaltool"
+
+python3 conventional_rag.py \
 --model ${MODEL} \
 --llm_endpoint_url ${LLMENDPOINT} \
 --filedir ${FILEDIR} \
