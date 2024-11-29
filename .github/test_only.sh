dce031b717c5e340cb051ab188755644b9db73eb
Wenxin Zhang <wenxin.zhang@intel.com>
2024-11-29 14:45:46 +0800

    clean test code
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/test_only.sh
+++
@@ -1,8 +0,0 @@
-#This is for test only
-docker images
-docker stop $(docker ps -a -q) # this is the dangerous cmd
-docker stop # this is harmless
-docker ps -a # this is harmless
-docker ps -q # this is harmless
-sudo rm -fr  # this is the dangerous cmd
-rm -fr # this is harmless
