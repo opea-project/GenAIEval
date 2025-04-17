428f8e6687589f9f336fe66e30b755f7058f0a64
Sun, Xuehao <xuehao.sun@intel.com>
2025-04-17 16:24:39 +0800

    fix report
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .github/workflows/scripts/models/collect_log.sh
+++ .github/workflows/scripts/models/collect_log.sh
@@ -5,34 +5,39 @@
 
 set -eo pipefail
 set -x
-source /GenAIEval/.github/workflows/scripts/change_color
+
 WORKSPACE="/GenAIEval"
 # get parameters
 PATTERN='[-a-zA-Z0-9_]*='
 PERF_STABLE_CHECK=true
 for i in "$@"; do
     case $i in
-        --datasets*)
-            datasets=`echo $i | sed "s/${PATTERN}//"`;;
-        --device=*)
-            device=`echo $i | sed "s/${PATTERN}//"`;;
-        --model=*)
-            model=`echo $i | sed "s/${PATTERN}//"`;;
-        --tasks=*)
-            tasks=`echo $i | sed "s/${PATTERN}//"`;;
-        *)
-            echo "Parameter $i not recognized."; exit 1;;
+    --datasets*)
+        datasets=$(echo $i | sed "s/${PATTERN}//")
+        ;;
+    --device=*)
+        device=$(echo $i | sed "s/${PATTERN}//")
+        ;;
+    --model=*)
+        model=$(echo $i | sed "s/${PATTERN}//")
+        ;;
+    --tasks=*)
+        tasks=$(echo $i | sed "s/${PATTERN}//")
+        ;;
+    *)
+        echo "Parameter $i not recognized."
+        exit 1
+        ;;
     esac
 done
 
 log_file="/log/${device}/${model}/${device}-${tasks}-${model}-${datasets}.log"
-$BOLD_YELLOW && echo "-------- Collect logs --------" && $RESET
-
+echo "::notice:: Collecting logs ......"
 echo "working in"
 pwd
 if [[ ! -f ${log_file} ]]; then
-    echo "${device};${model};${tasks};${datasets};;" >> ${WORKSPACE}/summary.log
+    echo "|${device}|${model}|${tasks}|${datasets}|NaN|" >>${WORKSPACE}/summary.log
 else
     acc=$(grep -Po "acc .*(\d+(\.\d+)?)" ${log_file} | awk -F "|" '{print $3}' | head -n 1 | sed 's/.*://;s/[^0-9.]//g')
-    echo "${device};${model};${tasks};${datasets};${acc};" >> ${WORKSPACE}/summary.log
+    echo "|${device}|${model}|${tasks}|${datasets}|${acc}|" >>${WORKSPACE}/summary.log
 fi
