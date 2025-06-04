834309be33208a02607e32534e41a45324c059b1
Li Gang <gang.g.li@intel.com>
2025-06-04 15:36:03 +0800

    Add OpenVINO Model Server benchmark for embeddings and rerank
    1. Improve embedservefixed to support
       OpenAI embeddings API
       OpenVINO Model Server embeddings
    2. Add coherererankfixed to support
       Cohere rerank API
       Jina rerank API
       OpenVINO Model Server rerank
    
    Signed-off-by: Li Gang <gang.g.li@intel.com>

--- evals/benchmark/stresscli/locust/embedservefixed.py
+++ evals/benchmark/stresscli/locust/embedservefixed.py
@@ -2,14 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import tokenresponse as token
+import os
 
+model_id = os.getenv("MODEL_NAME", "BAAI/bge-base-en-v1.5")
+ep_prefix = os.getenv("EP_PREFIX", "/v1")
 
 def getUrl():
-    return "/v1/embeddings"
+    return f"{ep_prefix}/embeddings"
 
 
 def getReqData():
     return {
+        "model": model_id,
         "input": "In a world where technology has advanced beyond our wildest dreams, humanity stands on the brink of a new era. The year is 2050, and artificial intelligence has become an integral part of everyday life. Autonomous vehicles zip through the streets, drones deliver packages with pinpoint accuracy, and smart homes anticipate every need of their inhabitants. But with these advancements come new challenges and ethical dilemmas. As society grapples with the implications of AI, questions about privacy, security, and the nature of consciousness itself come to the forefront. Amidst this backdrop, a new breakthrough in quantum computing promises to revolutionize the field even further."
     }
 
